ff4e3ea8c163222a1d3e21d30b24bedf9cd88e9f
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2018-11-27 12:50:15 +0300

    Update

---
+++ src/main/java/Lesson_7/Cat.java
@@ -0,0 +1,41 @@
+package Lesson_7;
+
+public class Cat {
+    String name;
+    public String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println(name + " " + color + " " + age);
+    }
+}
